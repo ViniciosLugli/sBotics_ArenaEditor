22a3802d2380af58169d8a3b0d67f1eee93c9fb1
Vinicios Lugli <vinicioslugli@gmail.com>
2020-12-20 02:04:35 -0300

    Degrade na visão de peças underline, adicionado filtro de local elevado nas rampas e corrigido a peças elevada não filtrada para superior / não existentes(gangorra)

--- dataCode/UnderLine.cs
+++ dataCode/UnderLine.cs
@@ -3,17 +3,31 @@ using System;
 
 public class UnderLine : Area2D{
 	private bool IsOverlaping = false;
-
+	private float ModulateObjective = 1f;
 	public override void _Process(float delta){
 		if(IsOverlaping){
+			ModulateObjective = 0.4f;
 			GetParent().GetNode<Sprite>("TileBase").Visible = false;
 			GetNode<Sprite>("UnderLineSprite").Visible = true;
-			GetParent().GetNode<Sprite>("Filter").Modulate = new Color(1, 1, 1, 0.6f);
-		}
-		else{
+			
+			if((GetParent().GetNode<Sprite>("Filter").Modulate.a - ModulateObjective) < 0.01f){
+				GetParent().GetNode<Sprite>("Filter").Modulate =
+				new Color(1, 1, 1, ModulateObjective);
+			}else{
+				GetParent().GetNode<Sprite>("Filter").Modulate =
+				new Color(1, 1, 1, (float)(GetParent().GetNode<Sprite>("Filter").Modulate.a-((GetParent().GetNode<Sprite>("Filter").Modulate.a - ModulateObjective)*3f*delta)));
+			}
+		}else{
+			ModulateObjective = 1f;
 			GetNode<Sprite>("UnderLineSprite").Visible = false;
 			GetParent().GetNode<Sprite>("TileBase").Visible = true;
-			GetParent().GetNode<Sprite>("Filter").Modulate = new Color(1, 1, 1, 1);
+			if((ModulateObjective - GetParent().GetNode<Sprite>("Filter").Modulate.a) < 0.01f){
+				GetParent().GetNode<Sprite>("Filter").Modulate =
+				new Color(1, 1, 1, ModulateObjective);
+			}else{
+				GetParent().GetNode<Sprite>("Filter").Modulate =
+					new Color(1, 1, 1, (float)(GetParent().GetNode<Sprite>("Filter").Modulate.a+((ModulateObjective - GetParent().GetNode<Sprite>("Filter").Modulate.a)*1.8f*delta)));
+			}
 		}
 	}
 	private void _on_Area_mouse_entered(){
